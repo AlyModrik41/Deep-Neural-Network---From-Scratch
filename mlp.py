dc0b12c1e0d60f893ee7029b7f73da60f2e1b02d
Ali Mohamed <alihashem4g@gmail.com>
2026-04-04 21:53:17 +0200

    updated

--- mlp.py
+++ mlp.py
@@ -78,7 +78,7 @@ df=pd.read_csv('df_encoded.csv')
 X=df.drop(columns=['Species'])
 y=df['Species']
 class_names = ['Adelie', 'Chinstrap', 'Gentoo']
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=42,stratify=y)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.4,random_state=42,stratify=y)
 
 # Users Input List
 st.markdown("""
